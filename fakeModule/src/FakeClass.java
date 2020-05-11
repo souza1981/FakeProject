d75ef3f1d9d7e913fb585db8498974cdc5dffaf4
André Luiz Machado de Souza <42772766+souza1981@users.noreply.github.com>
2020-05-11 09:22:52 -0300

    Update FakeClass.java
    Alteração no branch direto no GitHub

--- fakeModule/src/FakeClass.java
+++ fakeModule/src/FakeClass.java
@@ -2,5 +2,7 @@ public class FakeClass {
     public static void main(String[] args) {
         System.out.println("Linha 1");
         System.out.println("Linha branch fakebug_1");
+        System.out.println("Linha branch fakebug_1.1");
+        
     }
 }
