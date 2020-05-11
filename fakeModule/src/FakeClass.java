7c96fec1dc2dc2a955f6883106e16e88f847c980
André Luiz Machado de Souza <42772766+souza1981@users.noreply.github.com>
2020-05-11 09:34:33 -0300

    fakebranch commit 3

--- fakeModule/src/FakeClass.java
+++ fakeModule/src/FakeClass.java
@@ -6,6 +6,7 @@ public class FakeClass {
         System.out.println("Linha branch fakebug_2");
         System.out.println("Linha branch fakebug_2.1");
         System.out.println("Linha branch fakebug_2.2");
+        System.out.println("Linha branch fakebug_2.3");
         
     }
 }
