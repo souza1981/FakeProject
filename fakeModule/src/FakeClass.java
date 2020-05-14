c66e1390691331a62773679b7d37f48bb596546d
Andre Souza <andre.souza@mmsi.com>
2020-05-14 11:41:28 -0700

    Linha fabebug_9

--- fakeModule/src/FakeClass.java
+++ fakeModule/src/FakeClass.java
@@ -12,6 +12,7 @@ public class FakeClass {
         System.out.println("Linha branch fakebug_3.2");
         System.out.println("Linha branch fakebug_4");
         System.out.println("Linha branch fakebug_4.1");
+        System.out.println("Linha branch fakebug_9");
 
         
     }
